d77579da47d2a08af32796600fdd6b64d7372733
Yamato Kataoka <yk_time@icloud.com>
2019-11-16 08:46:06 +0900

    Display a second map next to the default one

--- object-oriented-programming-in-java/UCSDUnfoldingMaps/src/module1/HelloWorld.java
+++ object-oriented-programming-in-java/UCSDUnfoldingMaps/src/module1/HelloWorld.java
@@ -36,7 +36,7 @@ public class HelloWorld extends PApplet
 	UnfoldingMap map2;
 
 	public void setup() {
-		size(800, 600, P2D);  // Set up the Applet window to be 800x600
+		size(850, 600, P2D);  // Set up the Applet window to be 800x600
 		                      // The OPENGL argument indicates to use the 
 		                      // Processing library's 2D drawing
 		                      // You'll learn more about processing in Module 3
@@ -74,7 +74,10 @@ public class HelloWorld extends PApplet
 		// This line makes the map interactive
 		MapUtils.createDefaultEventDispatcher(this, map1);
 		
-		// TODO: Add code here that creates map2 
+		// TODO: Add code here that creates map2
+		map2 = new UnfoldingMap(this, 450, 50, 350, 500, provider);
+		map2.zoomAndPanTo(zoomLevel, new Location(35.6f, 139.7f));
+		MapUtils.createDefaultEventDispatcher(this, map2);
 		// Then you'll modify draw() below
 
 	}
@@ -84,6 +87,7 @@ public class HelloWorld extends PApplet
 		// So far we only draw map1...
 		// TODO: Add code so that both maps are displayed
 		map1.draw();
+		map2.draw();
 	}
 
 	
