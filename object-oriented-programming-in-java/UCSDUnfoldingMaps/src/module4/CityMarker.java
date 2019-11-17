a53d3035d90385bed5f7d047069fb266a50d7068
Yamato Kataoka <yk_time@icloud.com>
2019-11-17 18:47:02 +0900

    added triangle for CityMarker

--- object-oriented-programming-in-java/UCSDUnfoldingMaps/src/module4/CityMarker.java
+++ object-oriented-programming-in-java/UCSDUnfoldingMaps/src/module4/CityMarker.java
@@ -45,6 +45,8 @@ public class CityMarker extends SimplePointMarker {
 		// e.g. pg.rect(x, y, 10, 10) will draw a 10x10 square
 		// whose upper left corner is at position x, y
 		// Check out the processing documentation for more methods
+		pg.fill(66, 144, 245);
+		pg.triangle(x-TRI_SIZE, y+TRI_SIZE, x, y-TRI_SIZE, x+TRI_SIZE, y+TRI_SIZE);
 		
 		// Restore previous drawing style
 		pg.popStyle();
