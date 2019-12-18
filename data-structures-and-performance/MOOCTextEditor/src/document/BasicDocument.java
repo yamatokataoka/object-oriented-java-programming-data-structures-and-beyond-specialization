a17f186545acb293d84595332b52e246dd739e79
Yamato Kataoka <yk_time@icloud.com>
2019-12-18 09:50:46 +0900

    Add new test case for uppercase letters

--- data-structures-and-performance/MOOCTextEditor/src/document/BasicDocument.java
+++ data-structures-and-performance/MOOCTextEditor/src/document/BasicDocument.java
@@ -109,6 +109,7 @@ public class BasicDocument extends Document
 		testCase(new BasicDocument("Sentences?!"), 3, 1, 1);
 		testCase(new BasicDocument("Lorem ipsum dolor sit amet, qui ex choro quodsi moderatius, nam dolores explicari forensibus ad."),
 		         32, 15, 1);
+		testCase(new BasicDocument("HELLO WORLD"), 3, 2, 1);
 	}
 	
 }
