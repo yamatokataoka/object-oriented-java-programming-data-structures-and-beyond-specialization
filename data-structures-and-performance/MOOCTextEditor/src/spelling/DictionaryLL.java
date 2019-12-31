ecc5ed69c428d21a4f4c4756b41834a83680b0e9
Yamato Kataoka <yk_time@icloud.com>
2019-12-31 10:16:15 +0900

    Refactor DictionaryLL class

--- data-structures-and-performance/MOOCTextEditor/src/spelling/DictionaryLL.java
+++ data-structures-and-performance/MOOCTextEditor/src/spelling/DictionaryLL.java
@@ -21,7 +21,7 @@ public class DictionaryLL implements Dictionary
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
     public boolean addWord(String word) {
-    	word = word.toLowerCase();
+    	String wordLow = word.toLowerCase();
     	if (!dict.contains(word)) {
     		dict.add(word);
     		return true;
@@ -39,7 +39,7 @@ public class DictionaryLL implements Dictionary
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-    	s = s.toLowerCase();
+    	String sLow = s.toLowerCase();
     	return dict.contains(s);
     }
 
