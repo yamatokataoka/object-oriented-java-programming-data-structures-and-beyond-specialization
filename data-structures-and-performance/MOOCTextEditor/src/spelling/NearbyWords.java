99d61c7fc1fdbf1592c40152b3c13dbd050857b1
Yamato Kataoka <yk_time@icloud.com>
2020-01-02 13:02:17 +0900

    Implement deletions method

--- data-structures-and-performance/MOOCTextEditor/src/spelling/NearbyWords.java
+++ data-structures-and-performance/MOOCTextEditor/src/spelling/NearbyWords.java
@@ -98,7 +98,16 @@ public class NearbyWords implements SpellingSuggest {
 	 * @return
 	 */
 	public void deletions(String s, List<String> currentList, boolean wordsOnly ) {
-		// TODO: Implement this method
+		for(int index = 0; index < s.length(); index++){
+			StringBuffer sb = new StringBuffer(s);
+			sb.deleteCharAt(index);
+
+			if(!currentList.contains(sb.toString()) && 
+					(!wordsOnly||dict.isWord(sb.toString())) &&
+					!s.equals(sb.toString())) {
+				currentList.add(sb.toString());
+			}
+		}
 	}
 
 	/** Add to the currentList Strings that are one character deletion away
